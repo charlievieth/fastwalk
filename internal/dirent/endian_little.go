5449d1152aee030e449d67e387c6c350bddba1e4
Charlie Vieth <charlie.vieth@gmail.com>
2025-06-19 23:40:59 -0400

    internal/dirent: move arch dependent code to make codecov happy
    This is a pointless commit that simply moves the arch specific readInt*
    functions to their respective build tagged files. The goal here is to
    make codecov report a faired value (I don't really care about this, but
    some do so here we are).

--- internal/dirent/endian_little.go
+++ internal/dirent/endian_little.go
@@ -6,4 +6,30 @@
 
 package dirent
 
-const isBigEndian = false
+// readInt returns the size-bytes unsigned integer in native byte order at offset off.
+func readInt(b []byte, off, size uintptr) (uint64, bool) {
+	if len(b) < int(off+size) {
+		return 0, false
+	}
+	b = b[off:]
+	switch size {
+	case 1:
+		return uint64(b[0]), true
+	case 2:
+		_ = b[1] // bounds check hint to compiler; see golang.org/issue/14808
+		return uint64(b[0]) | uint64(b[1])<<8, true
+	case 4:
+		_ = b[3] // bounds check hint to compiler; see golang.org/issue/14808
+		return uint64(b[0]) | uint64(b[1])<<8 | uint64(b[2])<<16 | uint64(b[3])<<24, true
+	case 8:
+		_ = b[7] // bounds check hint to compiler; see golang.org/issue/14808
+		return uint64(b[0]) | uint64(b[1])<<8 | uint64(b[2])<<16 | uint64(b[3])<<24 |
+			uint64(b[4])<<32 | uint64(b[5])<<40 | uint64(b[6])<<48 | uint64(b[7])<<56, true
+	default:
+		// This case is impossible if the tests pass. Previously, we panicked
+		// here but for performance reasons (escape analysis) it's better to
+		// trigger a panic via an out-of-bounds reference.
+		_ = b[len(b)]
+		return 0, false
+	}
+}
